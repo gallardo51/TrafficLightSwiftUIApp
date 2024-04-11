f8908f28cabfd640463db5c4f3b09463cd0017f3
Alex Sobolev <31108@mail.ru>
2024-04-11 12:44:14 +0300

    Initial Commit

---
+++ TrafficLightSwiftUIApp/TrafficLightSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSwiftUIAppApp.swift
+//  TrafficLightSwiftUIApp
+//
+//  Created by Александр Соболев on 11.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
